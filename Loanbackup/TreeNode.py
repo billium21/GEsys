6d167acd8add31ebb8d4db635907264be36804f5
Tom Alberi <talberi@graisellsworth.com>
2013-05-17 17:22:02 -0400

    Added dynamically generated attributes on treenode.

--- Loanbackup/TreeNode.py
+++ Loanbackup/TreeNode.py
@@ -1,6 +1,7 @@
 # TreeNode Class
 import os
 
+
 class TreeNode:
 
     """
@@ -27,6 +28,7 @@ class TreeNode:
         for chld in self.lChild:
             print chld.Fname
 
+    #I think this is redundant of the leafnode attribute. Remove?
     def has_Chldrn(self):
         if len(self.lChild) > 0:
             return True
@@ -41,11 +43,26 @@ class TreeNode:
         #return result
         return [child.Fname for child in self.lChild if child.leafNode]
 
+    def __getattr__(self, name):
+        if name is 'leaf_names':
+            return [child.Fname for child in self.lChild if child.leafNode]
+        elif name is 'child_name_date':
+            return [(child.Fname, child.modDate) for child in self.lChild]
+        elif name is 'name_date':
+            return (self.Fname, self.modDate)
+        else:
+            raise AttributeError('%s not a valid attribute' % name)
+
     def build_path(self):
         if self.Parent:
             return self.Parent.build_path() + self.Fname + os.sep
         else:
             return self.Fname + os.sep
+
+
+#Depth-first traverse the tree and call visit function on each node.
+def df_traverse(root, visit_func):
+    pass
 
 
 if __name__ == '__main__':
@@ -54,12 +71,18 @@ if __name__ == '__main__':
     testNode2 = testNode.newChild("Test2")
     testNode3 = testNode.newChild("Test3")
     testNode4 = testNode3.newChild("Test4")
-    print testNode.lChild
+    #print testNode.lChild
     ##testNode.newChild("tChild1")
     ##testNode.newChild("tChild2")
     ##testNode.newChild("tChild3")
     ##
     ##testNode.list_Chldrn()
     ##print testNode3.build_path()
-    print testNode.get_leaves()
-    print testNode4.build_path()
+    #print testNode.get_leaves()
+    #print testNode4.build_path()
+
+    print testNode.leaf_names
+    print testNode.child_name_date
+    print testNode.name_date
+    #print testNode.blah
+
