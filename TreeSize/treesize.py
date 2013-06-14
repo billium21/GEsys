3e79b1b28b16c9fd399fe4ee3d8209cfaf9eb8d0
Tom Alberi <talberi@graisellsworth.com>
2013-06-14 12:27:58 -0400

    Greatly simplified and sped up the file data size rollup.

--- TreeSize/treesize.py
+++ TreeSize/treesize.py
@@ -3,14 +3,9 @@ import os
 import TreeNode
 from collections import deque
 
-
+@profile
 def build_tree_bf(node, searchpath):
-    #from itertools import count
-    #cnt = count()
-    dirs = None
     queue = deque()
-    depth = 1
-
     queue.appendleft((node, searchpath))
 
     while len(queue) > 0:
@@ -43,49 +38,24 @@ def build_tree_bf(node, searchpath):
                 newnode = qnode.newChild(dirx, depth=depth)
                 newnode.datasize = 0
                 newnode.fileflag = False
-                #if next(cnt) % 1000 == 0:
-                    #print cnt, newpath
                 queue.appendleft((newnode, newpath))
 
 
 #@profile
 def rollup_sizes(node):
-    import heapq
     leaflist = []
-    tracking = {}
-    heap = []
 
     def collectleaves(visitnode):
         if visitnode.leafNode:
             leaflist.append(visitnode)
 
     node.df_traverse(collectleaves)
-    leaflist = sorted(leaflist, key=lambda x: x.depth)
-    #print '; '.join([str((x.Fname, x.depth)) for x in leaflist])
-    queue = deque(leaflist)
 
-    while len(queue) > 0:
-        currnode = queue.pop()
-        if currnode.Parent is not None:
-            currnode.Parent.datasize += currnode.datasize
-            #if currnode.Parent not in queue:
-            if currnode.Parent not in tracking:
-                tracking[currnode.Parent] = None
-                #queue.appendleft(currnode.Parent)
-                heapq.heappush(heap, (10000 - currnode.Parent.depth, currnode.Parent))
-
-    while len(heap) > 0:
-        dp, currnode = heapq.heappop(heap)
-        if currnode.Parent is not None:
-            currnode.Parent.datasize += currnode.datasize
-            if currnode.Parent not in tracking:
-                tracking[currnode.Parent] = None
-                heapq.heappush(heap, (10000 - currnode.Parent.depth, currnode.Parent))
-
-    #for treenode in leaves:
-        #while treenode.Parent is not None:
-            #treenode.Parent.datasize += treenode.datasize
-            #treenode = treenode.Parent
+    for treenode in leaflist:
+        ds = treenode.datasize
+        while treenode.Parent is not None:
+            treenode.Parent.datasize += ds
+            treenode = treenode.Parent
 
 
 if __name__ == '__main__':
